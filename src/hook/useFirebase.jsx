13dfac8ebcfd025806b46adea108222fe516b537
mdakbarhossain1 <akbarshovo18@gmail.com>
2024-03-30 11:19:17 +0600

    google signin error

--- src/hook/useFirebase.jsx
+++ src/hook/useFirebase.jsx
@@ -1,8 +1,63 @@
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth"
+import { useEffect, useState } from "react";
+import firebaseAuthentication from "../Firebase/firebase.init";
 
+
+
+// Initialize Firebase
+// initializeApp(firebaseConfig);
+firebaseAuthentication();
 const useFirebase = () => {
-  return (
-    <div>useFirebase</div>
-  )
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const auth = getAuth();
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+
+    const signInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const googleSignIn = () => {
+        const googleProvide = new GoogleAuthProvider()
+        return signInWithPopup(auth, googleProvide)
+    }
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                setUser(user)
+                // ...
+            } else {
+                // User is signed out
+                // ...
+                setLoading(false);
+            }
+        });
+    }, [])
+
+    return {
+        user,
+        loading,
+        createUser,
+        signInUser,
+        logOut,
+        googleSignIn
+    }
+
 }
 
 export default useFirebase
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,77 @@
+import { Link } from "react-router-dom"
+import Button from "../../components/Form/Button"
+import TextField from "../../components/Form/TextField"
+import { useState } from "react"
+// import useAuth from "../../hook/useAuth"
+import GoogleSignIn from "../../components/Form/GoogleSignIn"
 
 
 const Login = () => {
+
+  // const { signInUser } = useAuth()
+
+  const [userInput, setUserInput] = useState({
+    email: '',
+    password: '',
+  })
+
+
+  //form inputs
+  const Inputs = [
+    { id: 1, type: "email", placeholder: "Email", value: `${userInput.email}`, name: 'email' },
+    { id: 2, type: "password", placeholder: "Password", value: `${userInput.password}`, name: 'password' },
+  ];
+
+  // handle change
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setUserInput(prev => {
+      return {
+        ...prev,
+        [name]: value
+      }
+    })
+  }
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(userInput: email, userInput: password)
+  // }
+
+  //handle submit form 
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(userInput: email, userInput: password)
+  //   signInUser(userInput: email, userInput: password)
+  // }
+
   return (
-    <div>Login</div>
+    <main className="h-screen w-full banner">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
+        <div className="flex flex-col justify-center items-center h-screen">
+          <form className="bg-white w-3/5 mt-6 p-4 rounded-lg shadow-lg" onSubmit={console.log("first")}>
+            <div className="flex flex-col space-y-6">
+              {Inputs.map(input => (
+                <TextField
+                  key={input.id}
+                  type={input.type}
+                  placeholder={input.placeholder}
+                  value={input.value}
+                  name={input.name}
+                  onChange={handleChange}
+                />
+              ))}
+            </div>
+            <Button text="Sign In" />
+            <Link to="/signup">
+              <p className="text-base text-primary text-center my-6 hover:underline">Need an account ?</p>
+            </Link>
+
+            <GoogleSignIn text="Sign In With Google" />
+          </form>
+        </div>
+      </div>
+    </main>
   )
 }
 
